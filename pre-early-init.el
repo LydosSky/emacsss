1168ea86d35f0fc70797e4b41162fe9ddf2c43c5
Lydos <lydossky@gmail.com>
2025-02-19 22:39:38 +0300

    feat: font face settings

--- pre-early-init.el
+++ pre-early-init.el
@@ -3,12 +3,18 @@
 
 ;;; Commentary:
 ;;; Code:
-;; Reducing clutter in ~/.emacs.d by redirecting files to ~/emacs.d/var/
+
+
+;; Start the timer at the very beginning of init.el
 (setq minimal-emacs-var-dir (expand-file-name "packages/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" minimal-emacs-var-dir))
 (setq user-emacs-directory minimal-emacs-var-dir)
 (setq custom-file null-device)
-
+(setq-default cursor-type '(bar . 3))
+(set-face-attribute 'default nil
+                     :family "Roboto Mono"
+                     :height 130
+                     :weight 'regular)
 (provide 'pre-ealy-init)
 ;;; pre-ealy-init.el ends here
 
