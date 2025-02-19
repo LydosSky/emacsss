2f6920317c7083ca48955d1b8b9519efa3a890d7
Lydos <lydossky@gmail.com>
2025-02-19 22:40:31 +0300

    feat: auto-completion with company

---
+++ modules/module-company.el
@@ -0,0 +1,47 @@
+;;; module-company.el --- Company mode configuration module -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Company is a modular text completion framework.
+;; This configuration sets up Company with various backends and enhancements.
+
+;;; Code:
+
+(use-package company
+  :defer t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-idle-delay 0.2)  ;; Delay before suggestions pop up
+  (setq company-minimum-prefix-length 2)  ;; Minimum number of characters before suggestions
+  (setq company-show-numbers t)  ;; Show numbers for suggestions
+  (setq company-tooltip-align-annotations t)  ;; Align annotations to the right tooltip border
+  (setq company-selection-wrap-around t)  ;; Wrap around suggestions
+  (global-set-key (kbd "M-/") 'company-complete)  ;; Keybinding for manual completion
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-common-or-cycle)  ;; Use tab for cycling completions
+              ("<backtab>" . company-select-previous)
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-show-single-candidate t)  ;; Show single candidate in tooltip
+  (setq company-box-backends-colors nil))  ;; Use default colors for backends
+
+(use-package company-quickhelp
+  :defer t
+  :after company
+  :hook (company-mode . company-quickhelp-mode)
+  :config
+  (setq company-quickhelp-delay 0.5))  ;; Delay before quickhelp popup
+
+(use-package company-statistics
+  :defer t
+  :after company
+  :config
+  (company-statistics-mode))  ;; Enable company-statistics mode
+
+(provide 'module-company)
+;;; module-company.el ends here
