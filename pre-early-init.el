0e301369960326e2f134d839f865f10952960fc6
Lydos <lydossky@gmail.com>
2025-02-19 21:22:18 +0300

    feat: straight.el configuration

--- pre-early-init.el
+++ pre-early-init.el
@@ -4,7 +4,7 @@
 ;;; Commentary:
 ;;; Code:
 ;; Reducing clutter in ~/.emacs.d by redirecting files to ~/emacs.d/var/
-(setq minimal-emacs-var-dir (expand-file-name "var/" minimal-emacs-user-directory))
+(setq minimal-emacs-var-dir (expand-file-name "packages/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" minimal-emacs-var-dir))
 (setq user-emacs-directory minimal-emacs-var-dir)
 (setq custom-file null-device)
