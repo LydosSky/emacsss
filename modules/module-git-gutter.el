062a25c5250d76f5a95cb6a081e8e067be640929
Lydos <lydossky@gmail.com>
2025-02-20 14:40:35 +0300

    feat: tty and gui setup for git-gutter

--- modules/module-git-gutter.el
+++ modules/module-git-gutter.el
@@ -9,16 +9,41 @@
 ;;; Code:
 
 (use-package git-gutter
-  :hook (prog-mode . git-gutter-mode)
   :ensure t
+  :hook
+  ((prog-mode . git-gutter-mode)
+   (text-mode . git-gutter-mode))
   :config
-  (setq git-gutter:update-interval 0.02))
-(use-package git-gutter-fringe
-  :ensure t
-  :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+  ;; Adjust update interval (in seconds)
+  (setq git-gutter:update-interval 0.2)
+  (unless (display-graphic-p)
+    (setq git-gutter:modified-sign"▎")
+    (setq git-gutter:added-sign   "▎")
+    (setq git-gutter:deleted-sign "▎")
+    
+    ;; Let git-gutter display in the margin
+    (setq git-gutter:visibility-indicator nil)
+    (setq git-gutter:window-width 1)
+    ;; Adjust where the indicators appear
+    (setq git-gutter:side 'left)))
+
+(when (display-graphic-p)
+  (use-package git-gutter-fringe
+    :ensure t
+    :demand t
+    :after git-gutter
+    :config
+    ;; Use slim vertical bars for indicators
+    (setq-default fringes-outside-margins t)
+    
+    (when (display-graphic-p)
+      ;; Adjust fringe width if necessary
+      ;; Define custom fringe bitmaps
+      (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+      (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+      (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+      ))
+  )
 
 
 (provide 'module-git-gutter)
