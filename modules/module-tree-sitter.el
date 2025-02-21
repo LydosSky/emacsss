722ef26cab9fc572fc1730e0c8a03d04997f8349
Lydos <lydossky@gmail.com>
2025-02-21 21:47:00 +0300

    feat: tree-sitter

--- modules/module-tree-sitter.el
+++ modules/module-tree-sitter.el
@@ -9,13 +9,15 @@
 ;; Tree-sitter core
 (use-package tree-sitter
   :ensure t
-  :config
-  (global-tree-sitter-mode))                      
+  :defer t
+  :config)
 
 ;; ;; Tree-sitter language bundles
 (use-package tree-sitter-langs
-   :ensure t
-   :after tree-sitter)
+  :ensure t
+  :defer t
+  :after tree-sitter)
+
 
 
 (provide 'module-tree-sitter)
