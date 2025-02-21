cc51708845d4b66ee145168a52264b44b5db05d7
Lydos <lydossky@gmail.com>
2025-02-21 21:45:12 +0300

    fix: flycheck tweaks

--- modules/module-flycheck.el
+++ modules/module-flycheck.el
@@ -10,7 +10,10 @@
   :defer t
   :init
   (global-flycheck-mode)
-
+  :config
+  ;; Additional configurations
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;; Other options: 'idle-change, 'new-line, 'idle-buffer-switch
   ;; Disable Flycheck's error notifications and symbols
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
   (setq flycheck-indication-mode 'right-fringe) ;; Or 'left-fringe or nil
@@ -20,16 +23,11 @@
     (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] nil nil 'center))
 
   ;; Optionally disable underlining of errors
-  (setq flycheck-highlighting-mode 'nil) ;; Options: 'symbols, 'lines, 'columns, or nil
+  (setq flycheck-highlighting-mode 'symbols) ;; Options: 'symbols, 'lines, 'columns, or nil
 
   ;; Disable tooltip pop-ups
   (setq flycheck-pos-tip-mode nil)
   (setq flycheck-display-errors-delay 0.3)
-
-  :config
-  ;; Additional configurations
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  ;; Other options: 'idle-change, 'new-line, 'idle-buffer-switch
   )
 
 (provide 'module-flycheck)
