6e8f20149ceb99ee2778783a17283fce5ec74dc3
Lydos <lydossky@gmail.com>
2025-02-27 13:11:39 +0300

    company-box

--- modules/module-company.el
+++ modules/module-company.el
@@ -11,9 +11,9 @@
   :defer t
   :hook (after-init . global-company-mode)
   :init
-  (setq company-idle-delay 0.2)  ;; Delay before suggestions pop up
+  (setq company-idle-delay 0.1)  ;; Delay before suggestions pop up
   (setq company-minimum-prefix-length 2)  ;; Minimum number of characters before suggestions
-  (setq company-show-numbers t)  ;; Show numbers for suggestions
+  (setq company-show-numbers nil)  ;; Show numbers for suggestions
   (setq company-tooltip-align-annotations t)  ;; Align annotations to the right tooltip border
   (setq company-selection-wrap-around t)  ;; Wrap around suggestions
   (setq company-require-match 'never)
@@ -21,6 +21,8 @@
   (setq company-dabbrev-other-buffers nil)
   (setq company-dabbrev-ignore-case nil)
   (setq company-dabbrev-downcase nil)
+  (setq company-tooltip-align-annotations t)
+  (setq company-tooltip-offset-display 'scroll)  ;; Can be 'lines', 'scrollbar', or nil
   (global-set-key (kbd "M-/") 'company-complete)  ;; Keybinding for manual completion
   :bind (:map company-active-map
               ("<tab>" . company-complete-common-or-cycle)  ;; Use tab for cycling completions
@@ -34,11 +36,50 @@
   :after company
   :config
   (company-statistics-mode))  ;; Enable company-statistics mode
+
 (setq-local completion-at-point-functions
             (mapcar #'cape-company-to-capf
                     (list #'company-files #'company-keywords #'company-dabbrev)))
-(setq company-tooltip-align-annotations t)
-(setq company-tooltip-offset-display 'lines)  ;; Can be 'lines', 'scrollbar', or nil
+
+(use-package company-box
+  :defer t
+  :hook (company-mode . company-box-mode)
+  :custom
+  (company-box-enable-icon t)
+  (company-box-scrollbar nil)
+  :init
+  ;; Define a list of letter icons
+  (setq company-box-icons-alist 'company-box-icons-letters)
+
+  (setq company-box-icons-letters
+        `((Unknown       . ,(propertize "U" 'face 'font-lock-comment-face))
+          (Text          . ,(propertize "T" 'face 'font-lock-string-face))
+          (Method        . ,(propertize "M" 'face 'font-lock-function-name-face))
+          (Function      . ,(propertize "ƒ" 'face 'font-lock-function-name-face))
+          (Constructor   . ,(propertize "C" 'face 'font-lock-type-face))
+          (Field         . ,(propertize "F" 'face 'font-lock-variable-name-face))
+          (Variable      . ,(propertize "V" 'face 'font-lock-variable-name-face))
+          (Class         . ,(propertize "C" 'face 'font-lock-type-face))
+          (Interface     . ,(propertize "I" 'face 'font-lock-type-face))
+          (Module        . ,(propertize "M" 'face 'font-lock-constant-face))
+          (Property      . ,(propertize "P" 'face 'font-lock-variable-name-face))
+          (Unit          . ,(propertize "U" 'face 'font-lock-constant-face))
+          (Value         . ,(propertize "V" 'face 'font-lock-builtin-face))
+          (Enum          . ,(propertize "E" 'face 'font-lock-builtin-face))
+          (Keyword       . ,(propertize "K" 'face 'font-lock-keyword-face))
+          (Snippet       . ,(propertize "S" 'face 'font-lock-string-face))
+          (Color         . ,(propertize "C" 'face 'font-lock-type-face))
+          (File          . ,(propertize "F" 'face 'font-lock-string-face))
+          (Reference     . ,(propertize "R" 'face 'font-lock-variable-name-face))
+          (Folder        . ,(propertize "D" 'face 'font-lock-doc-face))
+          (EnumMember    . ,(propertize "E" 'face 'font-lock-builtin-face))
+          (Constant      . ,(propertize "C" 'face 'font-lock-constant-face))
+          (Struct        . ,(propertize "S" 'face 'font-lock-type-face))
+          (Event         . ,(propertize "E" 'face 'font-lock-warning-face))
+          (Operator      . ,(propertize "O" 'face 'font-lock-comment-delimiter-face))
+          (TypeParameter . ,(propertize "T" 'face 'font-lock-type-face)))))
+
+
 
 (provide 'module-company)
 ;;; module-company.el ends here
